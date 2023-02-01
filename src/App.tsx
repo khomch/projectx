05505df1a4d4c0ecfad8e0bce897453189a7ec9f
Anton Khomchenko <khomchenko.anton@yandex.ru>
2023-02-01 10:56:07 +0000

    [FEAT]: theme change logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React, { Suspense } from 'react';
 import Counter from "./components/Counter";
-import './index.scss';
+import './styles/index.scss';
 import { Link, Route, Routes } from 'react-router-dom';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPageAsync';
+import { useTheme } from './theme/useTheme';
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={ `app ${ theme }` }>
       <Link to={ '/' }>Main</Link>
       <br/>
       <Link to={ '/about' }>About</Link>
+      <br/>
+      <button onClick={ toggleTheme }>change theme</button>
       <Suspense fallback={ <div>Loading...</div> }>
         <Routes>
           <Route path={ '/about' } element={ <AboutPageAsync/> }/>
