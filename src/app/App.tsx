d79557bab8315f8aa623d481222ca2742999d92c
Anton Khomchenko <khomchenko.anton@yandex.ru>
2023-02-06 13:54:08 +0000

    [FEAT]: router refactored

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,9 @@
-import React, { Suspense } from 'react';
+import React  from 'react';
 import './styles/index.scss';
-import { Link, Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { classNames } from 'shared/lib/helpers/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
+import { AppRouter } from 'app/providers/router';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -16,12 +15,7 @@ const App = () => {
       <Link to={ '/about' }>About</Link>
       <br/>
       <button onClick={ toggleTheme }>change theme</button>
-      <Suspense fallback={ <div>Loading...</div> }>
-        <Routes>
-          <Route path={ '/about' } element={ <AboutPage/> }/>
-          <Route path={ '/' } element={ <MainPage /> }/>
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
