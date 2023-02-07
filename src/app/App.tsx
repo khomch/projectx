cc97693b536faf37149acd95c7022cc4a8621d34
Anton Khomchenko <khomchenko.anton@yandex.ru>
2023-02-07 10:30:03 +0000

    [FEAT]: NavBar, AppLink added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,16 @@
 import React  from 'react';
 import './styles/index.scss';
-import { Link } from 'react-router-dom';
 import { classNames } from 'shared/lib/helpers/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
+import { Navbar } from 'widgets/Navbar';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <Link to={ '/' }>Main</Link>
-      <br/>
-      <Link to={ '/about' }>About</Link>
-      <br/>
+      <Navbar />
       <button onClick={ toggleTheme }>change theme</button>
       <AppRouter />
     </div>
