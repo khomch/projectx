4ea07fbf08209dc4d3f71633c69d9744334e2d96
Anton Khomchenko <khomchenko.anton@yandex.ru>
2023-02-08 14:17:16 +0000

    [FEAT]: Webpack hot module replacement added

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -15,6 +15,7 @@ export function buildPlugins({ paths, isDev }: TBuildOptions): webpack.WebpackPl
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev)
-    })
+    }),
+    new webpack.HotModuleReplacementPlugin()
   ]
 }
