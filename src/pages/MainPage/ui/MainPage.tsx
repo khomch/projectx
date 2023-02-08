ff51fd9b1ba78d171063b7568902f3e99a8f7dfd
Anton Khomchenko <khomchenko.anton@yandex.ru>
2023-02-08 14:04:03 +0000

    [FEAT]: i18n Rus & Eng added

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,6 +1,10 @@
+import { useTranslation } from 'react-i18next';
+
 const MainPage = () => {
+  const { t } = useTranslation('main');
+
   return (
-    <div>Main page</div>
+    <div>{ t('main-page') }</div>
   )
 }
 
