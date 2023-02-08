72ec61f19e79055ad9459309dc05223d877824f6
Anton Khomchenko <khomchenko.anton@yandex.ru>
2023-02-08 11:58:02 +0000

    [FEAT]: Sidebar. Layout added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,21 @@
-import React  from 'react';
+import React from 'react';
 import './styles/index.scss';
 import { classNames } from 'shared/lib/helpers/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
 
 const App = () => {
   const { theme } = useTheme();
 
   return (
-    <div className={classNames('app', {}, [theme])}>
-      <Navbar />
-      <AppRouter />
+    <div className={ classNames('app', {}, [ theme ]) }>
+      <Navbar/>
+      <div className={'content-page'}>
+        <Sidebar/>
+        <AppRouter/>
+      </div>
     </div>
   );
 };
