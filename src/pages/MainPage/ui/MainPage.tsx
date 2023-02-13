503e43fbefbe7ca795d8b129db9d4f59ca543d0e
Anton Khomchenko <khomchenko.anton@yandex.ru>
2023-02-13 11:01:54 +0000

    [FEAT]: ErrorBoundary added

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,10 +1,14 @@
 import { useTranslation } from 'react-i18next';
+import { BugButton } from 'app/providers/ErrorBoundary';
 
 const MainPage = () => {
     const { t } = useTranslation('main');
 
     return (
-        <div>{ t('main-page') }</div>
+        <div>
+            <BugButton />
+            { t('main-page') }
+        </div>
     );
 };
 
---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,22 @@
+import { classNames } from 'shared/lib/helpers/classNames/classNames';
+import { Button } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
+import cls from './PageError.module.scss';
+
+interface PageErrorProps {
+  className?: string
+}
+
+export const PageError = ({ className }: PageErrorProps) => {
+    const { t } = useTranslation();
+    const reloadPage = () => {
+        window.location.reload();
+    };
+
+    return (
+        <div className={classNames(cls.PageError, {}, [className])}>
+            <p>{t('page-error')}</p>
+            <Button onClick={reloadPage}>{t('button-reload-page')}</Button>
+        </div>
+    );
+};
