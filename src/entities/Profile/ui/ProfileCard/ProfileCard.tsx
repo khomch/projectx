b6945b918c43c398b705ccb967a38a86b6f673d0
Anton Khomchenko <khomchenko.anton@yandex.ru>
2023-03-04 17:57:26 +0000

    [FIX]: code cleaned

--- src/entities/Profile/ui/ProfileCard/ProfileCard.tsx
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.tsx
@@ -2,8 +2,6 @@ import { classNames } from 'shared/lib/helpers/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { getProfileData } from 'entities/Profile/model/selectors/getProfileData/getProfileData';
-import { getProfileIsLoading } from 'entities/Profile/model/selectors/getProfileIsLoading/getProfileIsLoading';
-import { getProfileError } from 'entities/Profile/model/selectors/getProfileError/getProfileError';
 import { Text } from 'shared/ui/Text/Text';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { Input } from 'shared/ui/Input/Input';
@@ -16,8 +14,6 @@ interface ProfileCardProps {
 export const ProfileCard = ({ className }: ProfileCardProps) => {
     const { t } = useTranslation('profile');
     const data = useSelector(getProfileData);
-    const isLoading = useSelector(getProfileIsLoading);
-    const error = useSelector(getProfileError);
 
     return (
         <div className={classNames(cls.ProfileCard, {}, [className])}>
