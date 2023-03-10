5d760b9e9227e6722a58a89f02c2bba67d5c1e05
Anton Khomchenko <khomchenko.anton@yandex.ru>
2023-03-10 09:02:38 +0000

    [FEAT]: add third color to theme

--- src/app/providers/ThemeProvider/lib/useTheme.tsx
+++ src/app/providers/ThemeProvider/lib/useTheme.tsx
@@ -8,7 +8,21 @@ type TUseThemeResult = {
 
 export function useTheme(): TUseThemeResult {
     const { theme, setTheme } = useContext(ThemeContext);
-    const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
+    let newTheme: Theme;
+    switch (theme) {
+    case Theme.DARK:
+        newTheme = Theme.LIGHT;
+        break;
+    case Theme.LIGHT:
+        newTheme = Theme.BLUE;
+        break;
+    case Theme.BLUE:
+        newTheme = Theme.DARK;
+        break;
+    default:
+        newTheme = Theme.LIGHT;
+    }
+
     const toggleTheme = () => {
         setTheme?.(newTheme);
         localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
