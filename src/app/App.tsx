2d1808f7f2e8c6fb635bc689458d1b5ae288ffb3
Anton Khomchenko <khomchenko.anton@yandex.ru>
2023-03-13 12:05:27 +0000

    [FEAT]: protected routes added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,12 +4,13 @@ import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 import { useTheme } from 'app/providers/ThemeProvider';
-import { useDispatch } from 'react-redux';
-import { userActions } from 'entities/User';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserInitialized, userActions } from 'entities/User';
 
 const App = () => {
     const { theme } = useTheme();
     const dispatch = useDispatch();
+    const initialized = useSelector(getUserInitialized);
 
     useEffect(() => {
         dispatch(userActions.initAuthData());
@@ -21,7 +22,7 @@ const App = () => {
                 <Navbar />
                 <div className="content-page">
                     <Sidebar />
-                    <AppRouter />
+                    { initialized && <AppRouter /> }
                 </div>
             </Suspense>
         </div>
